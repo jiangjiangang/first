15a53b681423328e017d367141674cff1bfdb229
jiangjiangang <jiangjiangang@users.noreply.github.com>
2019-10-28 21:04:20 +0800

    Revert "home_end"
    This reverts commit dc969062326381699fce57f810e06a04a520d7a2.

--- AXF/static/axf/main/js/home.js
+++ AXF/static/axf/main/js/home.js
@@ -1,17 +1,7 @@
 $(function () {
-    initTopSwiper();
-    initSwiperMenu();
-})
-
-function initTopSwiper() {
     var swiper = new Swiper("#topSwiper", {
         loop: true,
         autoplay: 1000,
         pagination: '.swiper-pagination'
     });
-}
-function initSwiperMenu() {
-    var swiper = new Swiper("#swiperMenu", {
-        slidsPerView:3,
-    });
-}
+})
