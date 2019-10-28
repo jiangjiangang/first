dc969062326381699fce57f810e06a04a520d7a2
jiangjiangang <jiangjiangang@users.noreply.github.com>
2019-10-28 16:53:55 +0800

    home_end
    home_end
    market_start

--- AXF/app/views.py
+++ AXF/app/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, redirect
 from django.template import loader
 from django.urls import reverse
 from AXF.settings import MEDIA_KEY_PREFIX, EMAIL_HOST_USER
-from app.models import AXFUser, MainWheel
+from app.models import AXFUser, MainWheel, MainNav, MainMustBuy, MainShop, MainShow, FoodType
 from app.views_constant import HTTP_OK, HTTP_USER_EXIST, send_email_active
 
 
@@ -107,15 +107,34 @@ def active(request):
 
 def home(request):
     main_wheels = MainWheel.objects.all()
+    main_navs = MainNav.objects.all()
+    main_mustbuys = MainMustBuy.objects.all()
+    main_shop = MainShop.objects.all()
+    main_shop0_1 = main_shop[0:1]
+    main_shop1_3 = main_shop[1:3]
+    main_shop3_7 = main_shop[3:7]
+    main_shop7_11 = main_shop[7:11]
+    main_shows = MainShow.objects.all()
     data = {
         'title': '首页',
         'main_wheels': main_wheels,
+        'main_navs': main_navs,
+        'main_mustbuys': main_mustbuys,
+        'main_shop0_1': main_shop0_1,
+        'main_shop1_3': main_shop1_3,
+        'main_shop3_7': main_shop3_7,
+        'main_shop7_11': main_shop7_11,
+        'main_shows': main_shows,
     }
     return render(request, 'main/home.html', context=data)
 
 
 def market(request):
-    return render(request, 'main/market.html')
+    foodtypes = FoodType.objects.all()
+    data = {
+        'title': '闪购',
+    }
+    return render(request, 'main/market.html', context=data)
 
 
 def cart(request):
